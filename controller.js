6832e30af1a4b9b81aaa842f298bf00a65a5ed0b
dgcp3 <zdagmawig1@gmail.com>
2022-05-01 23:22:10 +0300

    remove log

--- controller.js
+++ controller.js
@@ -1,7 +1,6 @@
 import { projectData } from "./server";
 
 export function saveData(req, res) {
-  console.log(req.body);
   projectData.data.push({ ...req.body });
   res.status(201).send(projectData);
 }
