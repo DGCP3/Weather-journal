2852007927596e14f76ea7037949ce6991c0c5de
dgcp3 <zdagmawig1@gmail.com>
2022-05-01 19:07:03 +0300

    feat: :rocket: server

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-import cors from "cors";
 import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
 import { getData, saveData } from "./controller.js";
 const app = express();
-export let projectData = [];
+export let projectData = { data: [] };
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static("website"));
-
+const port = process.env.PORT || 3000;
 app.route("/weather").get(getData).post(saveData);
-app.listen(3000, () => console.log(`Listening on port 3000`));
+app.listen(port, () => console.log(`Listening on port ${port}`));
